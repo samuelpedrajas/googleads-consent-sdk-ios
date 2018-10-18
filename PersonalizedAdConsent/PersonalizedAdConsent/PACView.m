61def76676b6907fd85ece1f97b4960a8b1eb294
Samuel P <samuel.p991@gmail.com>
2018-10-19 01:21:29 +0200

    try to fix string comparison

--- PersonalizedAdConsent/PersonalizedAdConsent/PACView.m
+++ PersonalizedAdConsent/PersonalizedAdConsent/PACView.m
@@ -207,25 +207,25 @@ PACQueryParametersFromURL(NSURL *_Nonnull URL) {
   NSURL *URL = NULL;
   NSString * language = [[NSLocale preferredLanguages] firstObject];
 
-  if (language.isEqualToString("cn")) {
+  if ([language isEqualToString:"cn"]) {
     URL = [resourceBundle URLForResource:@"consentform_cn" withExtension:@"html"];
-  } else if (language.isEqualToString("de")) {
+  } else if ([language isEqualToString:"de"]) {
     URL = [resourceBundle URLForResource:@"consentform_de" withExtension:@"html"];
-  } else if (language.isEqualToString("es")) {
+  } else if ([language isEqualToString:"es"]) {
     URL = [resourceBundle URLForResource:@"consentform_es" withExtension:@"html"];
-  } else if (language.isEqualToString("fr")) {
+  } else if ([language isEqualToString:"fr"]) {
     URL = [resourceBundle URLForResource:@"consentform_fr" withExtension:@"html"];
-  } else if (language.isEqualToString("it")) {
+  } else if ([language isEqualToString:"it"]) {
     URL = [resourceBundle URLForResource:@"consentform_it" withExtension:@"html"];
-  } else if (language.isEqualToString("ja")) {
+  } else if ([language isEqualToString:"ja"]) {
     URL = [resourceBundle URLForResource:@"consentform_ja" withExtension:@"html"];
-  } else if (language.isEqualToString("ko")) {
+  } else if ([language isEqualToString:"ko"]) {
     URL = [resourceBundle URLForResource:@"consentform_ko" withExtension:@"html"];
-  } else if (language.isEqualToString("nl")) {
+  } else if ([language isEqualToString:"nl"]) {
     URL = [resourceBundle URLForResource:@"consentform_nl" withExtension:@"html"];
-  } else if (language.isEqualToString("pt")) {
+  } else if ([language isEqualToString:"pt"]) {
     URL = [resourceBundle URLForResource:@"consentform_pt" withExtension:@"html"];
-  } else if (language.isEqualToString("ru")) {
+  } else if ([language isEqualToString:"ru"]) {
     URL = [resourceBundle URLForResource:@"consentform_ru" withExtension:@"html"];
   } else {
     URL = [resourceBundle URLForResource:@"consentform" withExtension:@"html"];
